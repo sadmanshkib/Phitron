f3ece2a9b3e406e57a50d96b5f1e3dd98bbc1948
sadmanshkib <sadmanshkib@gmail.com>
2026-02-22 14:49:18 +0800

    02-22

--- XPSC/16-02-26 Codeforces Div. 3 Contest/B_Heapify_1.cpp
+++ XPSC/16-02-26 Codeforces Div. 3 Contest/B_Heapify_1.cpp
@@ -8,41 +8,52 @@ int main()
     {
         int n;
         cin >> n;
-        int a[n];
-        for (int i = 0; i < n; i++)
+        int a[n + 1];
+        for (int i = 1; i < n + 1; i++)
         {
             cin >> a[i];
         }
 
-        for (int i = 0; i < n; i++)
-        {
-            if (i + 2 < n)
-            {
-                if (a[i] > a[i + 2])
-                {
-                    swap(a[i], a[i + 2]);
-                }
-            }
-        }
-
-        bool flag = true;
-
-        // for (int i = 0; i < n; i++)
+        // for (int i = 1; i < n + 1; i++)
         // {
         //     cout << a[i] << " ";
         // }
         // cout << endl;
 
-        for (int i = 0; i < n - 1; i++)
+        for (int i = 1; i <= n / 2; i++)
+        {
+            // cout << a[i] << " " << a[i * 2] << endl;
+            for (int j = i * 2; j <= n; j *= 2)
+            {
+                if (a[i] > a[j])
+                {
+                    swap(a[i], a[j]);
+                }
+            }
+
+            // cout << a[i] << " " << a[i * 2] << endl;
+        }
+
+        bool flag = true;
+
+        // for (int i = 1; i < n + 1; i++)
+        // {
+        //     cout << a[i] << " ";
+        // }
+        // cout << endl;
+
+        for (int i = 1; i < n; i++)
         {
             if (a[i] > a[i + 1])
             {
                 flag = false;
+
+                // cout << a[i] << " " << a[i + 1];
                 break;
             }
         }
 
-        if(flag)
+        if (flag)
             cout << "YES" << endl;
         else
             cout << "NO" << endl;
